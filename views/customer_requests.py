417823b34d8978e77e874d59738bed4bece97eb7
Erin Stephens <erebstephens@gmail.com>
2023-04-24 20:01:22 -0500

    completed animal queries with SQL

--- views/customer_requests.py
+++ views/customer_requests.py
@@ -1,3 +1,7 @@
+import sqlite3
+import json
+from models import Customer
+
 CUSTOMERS = [
     {
         "id": 1,
@@ -7,54 +11,95 @@ CUSTOMERS = [
 
 def get_all_customers():
     '''gets all the customers'''
-    return CUSTOMERS
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name
+        FROM customer a
+        """)
+
+        # Initialize an empty list to hold all customer representations
+        customers = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an customer instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # customer class above.
+            customer = Customer(row['id'], row['name'])
+
+            customers.append(customer.__dict__)
+            # see the notes below for an explanation on this line of code.
+
+    return customers
 
 # Function with a single parameter
 def get_single_customer(id):
     '''gets single customer'''
-    # Variable to hold the found customer, if it exists
-    requested_customer = None
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    # Iterate the customers list above. Very similar to the
-    # for..of loops you used in JavaScript.
-    for customer in CUSTOMERS:
-        # Dictionaries in Python use [] notation to find a key
-        # instead of the dot notation that JavaScript used.
-        if customer["id"] == id:
-            requested_customer = customer
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name
+        FROM customer a
+        WHERE a.id = ?
+        """, ( id, ))
 
-    return requested_customer
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an customer instance from the current row
+        customer = Customer(data['id'], data['name'])
+
+        return customer.__dict__
 
 def create_customer(customer):
-    '''makes a new animal'''
-    # Get the id value of the last animal in the list
+    '''makes a new customer'''
+    # Get the id value of the last customer in the list
     max_id = CUSTOMERS[-1]["id"]
 
     # Add 1 to whatever that number is
     new_id = max_id + 1
 
-    # Add an `id` property to the animal dictionary
+    # Add an `id` property to the customer dictionary
     customer["id"] = new_id
 
-    # Add the animal dictionary to the list
+    # Add the customer dictionary to the list
     CUSTOMERS.append(customer)
 
     # Return the dictionary with `id` property added
     return customer
 
 def delete_customer(id):
-    '''deletes animal'''
-    # Initial -1 value for animal index, in case one isn't found
+    '''deletes customer'''
+    # Initial -1 value for customer index, in case one isn't found
     customer_index = -1
 
-    # Iterate the ANIMALS list, but use enumerate() so that you
+    # Iterate the customerS list, but use enumerate() so that you
     # can access the index value of each item
     for index, customer in enumerate(CUSTOMERS):
         if customer["id"] == id:
-            # Found the animal. Store the current index.
+            # Found the customer. Store the current index.
             customer_index = index
 
-    # If the animal was found, use pop(int) to remove it from list
+    # If the customer was found, use pop(int) to remove it from list
     if customer_index >= 0:
         CUSTOMERS.pop(customer_index)
 
--- views/employee_requests.py
+++ views/employee_requests.py
@@ -1,3 +1,7 @@
+import sqlite3
+import json
+from models import Employee
+
 EMPLOYEES = [
     {
         "id": 1,
@@ -7,54 +11,96 @@ EMPLOYEES = [
 
 def get_all_employees():
     '''gets all the employees'''
-    return EMPLOYEES
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name
+        FROM employee a
+        """)
+
+        # Initialize an empty list to hold all employee representations
+        employees = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an employee instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # employee class above.
+            employee = Employee(row['id'], row['name'])
+
+            employees.append(employee.__dict__)
+            # see the notes below for an explanation on this line of code.
+
+    return employees
 
 # Function with a single parameter
 def get_single_employee(id):
     '''gets single employee'''
-    # Variable to hold the found employee, if it exists
-    requested_employee = None
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    # Iterate the EMPLOYEES list above. Very similar to the
-    # for..of loops you used in JavaScript.
-    for employee in EMPLOYEES:
-        # Dictionaries in Python use [] notation to find a key
-        # instead of the dot notation that JavaScript used.
-        if employee["id"] == id:
-            requested_employee = employee
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name
+        FROM employee a
+        WHERE a.id = ?
+        """, ( id, ))
 
-    return requested_employee
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an employee instance from the current row
+        employee = Employee(data['id'], data['name'])
+
+        return employee.__dict__
 
 def create_employee(employee):
-    '''makes a new animal'''
-    # Get the id value of the last animal in the list
+    '''makes a new employee'''
+    # Get the id value of the last employee in the list
     max_id = EMPLOYEES[-1]["id"]
 
     # Add 1 to whatever that number is
     new_id = max_id + 1
 
-    # Add an `id` property to the animal dictionary
+    # Add an `id` property to the employee dictionary
     employee["id"] = new_id
 
-    # Add the animal dictionary to the list
+    # Add the employee dictionary to the list
     EMPLOYEES.append(employee)
 
     # Return the dictionary with `id` property added
     return employee
 
 def delete_employee(id):
-    '''deletes animal'''
-    # Initial -1 value for animal index, in case one isn't found
+    '''deletes employee'''
+    # Initial -1 value for employee index, in case one isn't found
     employee_index = -1
 
-    # Iterate the ANIMALS list, but use enumerate() so that you
+    # Iterate the employeeS list, but use enumerate() so that you
     # can access the index value of each item
     for index, employee in enumerate(EMPLOYEES):
         if employee["id"] == id:
-            # Found the animal. Store the current index.
+            # Found the employee. Store the current index.
             employee_index = index
 
-    # If the animal was found, use pop(int) to remove it from list
+    # If the employee was found, use pop(int) to remove it from list
     if employee_index >= 0:
         EMPLOYEES.pop(employee_index)
 
